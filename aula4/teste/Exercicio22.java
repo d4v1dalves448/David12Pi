e890e142c6a8506f7736882601e64ef26507510f
Simão Dias <105567918+simaodiazz@users.noreply.github.com>
2024-10-11 16:24:45 +0100

    Exercicio22

--- aula4/teste/Exercicio22.java
+++ aula4/teste/Exercicio22.java
@@ -1,5 +1,22 @@
 package teste;
 
 public class Exercicio22 {
-    
+    public static void main(String[] args) {
+       mensagemDeBoasVindas();
+       menu(); 
+    }
+
+    // Método para exibir o menu
+    public static void menu() {
+        System.out.println("== Menu =="); 
+        System.out.println("1- Entradas");
+        System.out.println("2- Prato Carne");
+        System.out.println("3- Prato Peixe");
+        System.out.println("4- Sobremesa");
+        System.out.println("5- Sair");
+    }
+    // Método para exibir o menu
+    public static void mensagemDeBoasVindas() {
+        System.out.println("Seja Bem Vindo"); 
+    }
 }
