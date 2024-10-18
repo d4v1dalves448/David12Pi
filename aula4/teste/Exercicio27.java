155910ca80e3c8e49da5a08e30f07f04a7fc06a0
Simão Dias <105567918+simaodiazz@users.noreply.github.com>
2024-10-18 16:07:31 +0100

    Exercicio27

---
+++ aula4/teste/Exercicio27.java
@@ -0,0 +1,15 @@
+package teste;
+
+public class Exercicio27 {
+    public static void main(String[] args) {
+
+        String idade = "30";
+
+        int a = Integer.parseInt(idade);
+        float y = Float.parseFloat(idade);
+
+
+        System.out.println("Valor Int: " + a );
+        System.out.println("Valor Float: " + y );
+    }
+}
